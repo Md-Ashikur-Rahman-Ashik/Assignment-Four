9f99611468a3c12e554e205f44c2cc634be189f2
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-02-08 10:23:02 +0600

    Alhamdulillah, third problem solved

---
+++ thirdProblem.js
@@ -0,0 +1,16 @@
+function deleteInvalids(arrays){
+    if(!Array.isArray(arrays)){
+        return "Please provide an array";
+    }
+
+    const final = [];
+    for(const array of arrays){
+        if(isNaN(array)){
+            continue;
+        }
+        else if(typeof array === "number"){
+            final.push(array);
+        }
+    }
+    return final;
+}
