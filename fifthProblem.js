68bd6a9df37706f710a21622b509cc26ee0ca5bf
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-02-08 12:13:37 +0600

    Assignment finished

--- fifthProblem.js
+++ fifthProblem.js
@@ -1,20 +1,19 @@
-function monthlySavings(arr, livingCost){
-    if(!Array.isArray(arr) || typeof livingCost !== "number"){
-        return "Invalid Input";
-    }
+function monthlySavings(arr, livingCost) {
+  if (!Array.isArray(arr) || typeof livingCost !== "number") {
+    return "Invalid Input";
+  }
 
-    let incomeAfterTax = 0;
-    for(let income of arr){
-        if(income >= 3000){
-            income = income - (income * 20 / 100);
-        }
-        incomeAfterTax = incomeAfterTax + income;
+  let incomeAfterTax = 0;
+  for (let income of arr) {
+    if (income >= 3000) {
+      income = income - (income * 20) / 100;
     }
+    incomeAfterTax = incomeAfterTax + income;
+  }
 
-    let saving = incomeAfterTax - livingCost;
-    if(saving < 0){
-        return "earn more";
-    }
-    return saving;
-
+  let saving = incomeAfterTax - livingCost;
+  if (saving < 0) {
+    return "earn more";
+  }
+  return saving;
 }
--- fourthProblem.js
+++ fourthProblem.js
@@ -1,17 +1,17 @@
-function password(obj){
-    if(!obj.name || !obj.birthYear || !obj.siteName){
-        return "invalid";
-    }
+function password(obj) {
+  if (!obj.name || !obj.birthYear || !obj.siteName) {
+    return "invalid";
+  }
 
-    const year = obj["birthYear"];
-    if(year < 1000){
-        return "invalid";
-    }
+  const year = obj["birthYear"];
+  if (year < 1000) {
+    return "invalid";
+  }
 
-    const website = obj["siteName"];
-    const modifyFirst = website.charAt(0);
-    const finalWord = modifyFirst.toUpperCase() + website.slice(1);
+  const website = obj["siteName"];
+  const modifyFirst = website.charAt(0);
+  const finalWord = modifyFirst.toUpperCase() + website.slice(1);
 
-    const createdPass = finalWord + "#" + obj["name"] + "@" + obj["birthYear"];
-    return createdPass;
+  const createdPass = finalWord + "#" + obj["name"] + "@" + obj["birthYear"];
+  return createdPass;
 }
