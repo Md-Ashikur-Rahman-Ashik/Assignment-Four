e227f66ec6f033d4d5cb9e162c66e039b25ddafe
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-02-08 11:16:35 +0600

    Solved second problem

--- secondProblem.js
+++ secondProblem.js
@@ -1,1 +1,20 @@
-// console.log(typeof NaN);
+function checkName(name) {
+  if (typeof name !== "string") {
+    return "invalid";
+  }
+
+  const check = name.slice(-1);
+  const convert = check.toLowerCase();
+
+  if (convert.includes("a") || convert.includes("y")) {
+    return "Good Name";
+  } else if (convert.includes("i") || convert.includes("e")) {
+    return "Good Name";
+  } else if (convert.includes("o") || convert.includes("u")) {
+    return "Good Name";
+  } else if (convert.includes("w")) {
+    return "Good Name";
+  } else {
+    return "Bad Name";
+  }
+}
